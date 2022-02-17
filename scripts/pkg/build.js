af5b4696c5203ca120a03054d8d4ce8316b68714
Piotr Grzesik <pj.grzesik@gmail.com>
2022-02-17 13:58:43 +0100

    chore: Add script for building binary release

---
+++ scripts/pkg/build.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const spawn = require('child-process-ext/spawn');
+const fse = require('fs-extra');
+
+const componentsPath = path.join(__dirname, '../..');
+const spawnOptions = { cwd: componentsPath, stdio: 'inherit' };
+
+(async () => {
+  // To bundle npm with a binary we need to install it
+  process.stdout.write('Install npm\n');
+  // Hard code npm version to one that comes with lastest Node.js
+  // It's due to fact that npm tends to issue buggy releases
+  // Node.js confirms on given version before including it within its bundle
+  // Version mappings reference: https://nodejs.org/en/download/releases/
+  await spawn('npm', ['install', '--no-save', 'npm@8.1.2'], spawnOptions);
+
+  try {
+    process.stdout.write('Build binaries\n');
+    await spawn(
+      'node',
+      [
+        './node_modules/.bin/pkg',
+        '--targets',
+        'node16-linux-x64,node16-mac-x64',
+        '--out-path',
+        'dist',
+        'bin/bin',
+      ],
+      spawnOptions
+    );
+  } finally {
+    await fse.remove(path.join(serverlessPath, 'node_modules/npm'));
+  }
+})();
