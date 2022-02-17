6435712cb2bb3c0e18028caf5e9be73fa05a538c
Piotr Grzesik <pj.grzesik@gmail.com>
2022-02-17 14:43:03 +0100

    refactor: Remove obsolete `load` util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const fs = require('./fs');
-const load = require('./load');
 const sleep = require('./sleep');
 const randomId = require('./randomId');
 
 module.exports = {
   ...fs,
-  load,
   sleep,
   randomId,
 };
