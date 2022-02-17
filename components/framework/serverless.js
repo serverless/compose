0c76a5cef165de986dd686b2608820633819be3b
Piotr Grzesik <pj.grzesik@gmail.com>
2022-02-17 17:24:06 +0100

    chore: Run prettier on serverless component

--- components/framework/serverless.js
+++ components/framework/serverless.js
@@ -1,74 +1,53 @@
-const Component = require("../../src/Component");
-const child_process = require("child_process");
-const yaml = require("yaml");
+const Component = require('../../src/Component');
+const child_process = require('child_process');
+const yaml = require('yaml');
 
 class ServerlessFramework extends Component {
   async deploy() {
-    const { stderr: deployOutput } = await this.exec("serverless", ["deploy"]);
-    if (deployOutput.includes("No changes to deploy. Deployment skipped.")) {
+    const { stderr: deployOutput } = await this.exec('serverless', ['deploy']);
+    if (deployOutput.includes('No changes to deploy. Deployment skipped.')) {
       return;
     }
 
-    const { stdout: infoOutput } = await this.exec("serverless", ["info"]);
+    const { stdout: infoOutput } = await this.exec('serverless', ['info']);
     let outputs;
     try {
       outputs = yaml.parse(infoOutput);
     } catch (e) {
-      throw new Error(
-        `Impossible to parse the output of "serverless info":\n${infoOutput}`
-      );
+      throw new Error(`Impossible to parse the output of "serverless info":\n${infoOutput}`);
     }
     await this.updateOutputs(outputs);
   }
 
   async remove() {
-    await this.exec("serverless", ["remove"]);
+    await this.exec('serverless', ['remove']);
     this.state = {};
     await this.save();
     await this.updateOutputs({});
   }
 
   async logs() {
-    const promises = Object.keys(this.outputs.functions).map(
-      async (functionName) => {
-        try {
-          await this.exec(
-            "serverless",
-            ["logs", "--function", functionName],
-            true
-          );
-        } catch (e) {
-          // TODO implement warning?
-                this.logVerbose(
-            `Error fetching logs for function ${this.id}:${functionName}`
-          );
-        }
+    const promises = Object.keys(this.outputs.functions).map(async (functionName) => {
+      try {
+        await this.exec('serverless', ['logs', '--function', functionName], true);
+      } catch (e) {
+        // TODO implement warning?
+        this.logVerbose(`Error fetching logs for function ${this.id}:${functionName}`);
       }
-    );
+    });
     await Promise.all(promises);
   }
 
   async dev() {
     Object.keys(this.outputs.functions).forEach((functionName) => {
-      this.exec(
-        "serverless",
-        ["logs", "--tail", "--function", functionName],
-        true
-      );
+      this.exec('serverless', ['logs', '--tail', '--function', functionName], true);
     });
 
     return this.context.watch(this.inputs.path, async () => {
-      this.context.status("Uploading");
-      const promises = Object.keys(this.outputs.functions).map(
-        async (functionName) => {
-          await this.exec("serverless", [
-            "deploy",
-            "function",
-            "--function",
-            functionName,
-          ]);
-        }
-      );
+      this.context.status('Uploading');
+      const promises = Object.keys(this.outputs.functions).map(async (functionName) => {
+        await this.exec('serverless', ['deploy', 'function', '--function', functionName]);
+      });
       await Promise.all(promises);
     });
   }
@@ -78,42 +57,43 @@ class ServerlessFramework extends Component {
    */
   async exec(command, args, streamStdout = false) {
     // Add stage
-    args.push("--stage", this.context.stage);
+    args.push('--stage', this.context.stage);
     // Add inputs
     for (const [key, value] of Object.entries(this.inputs?.parameters ?? {})) {
-      args.push("--param", `${key}=${value}`);
+      args.push('--param', `${key}=${value}`);
     }
 
-    this.logVerbose(`Running "${command} ${args.join(" ")}"`);
+    this.logVerbose(`Running "${command} ${args.join(' ')}"`);
     return new Promise((resolve, reject) => {
       const process = child_process.spawn(command, args, {
         cwd: this.inputs.path,
-        stdio: streamStdout ? "inherit" : undefined,
+        stdio: streamStdout ? 'inherit' : undefined,
       });
-      let stdout = "";
-      let stderr = "";
-      let allOutput = "";
+      let stdout = '';
+      let stderr = '';
+      let allOutput = '';
       if (process.stdout) {
-        process.stdout.on("data", (data) => {
+        process.stdout.on('data', (data) => {
           this.logVerbose(data.toString().trim());
           stdout += data;
           allOutput += data;
         });
       }
       if (process.stderr) {
-        process.stderr.on("data", (data) => {
+        process.stderr.on('data', (data) => {
           this.logVerbose(data.toString().trim());
           stderr += data;
           allOutput += data;
         });
       }
-      process.on("error", (err) => reject(err));
-      process.on("close", (code) => {
+      process.on('error', (err) => reject(err));
+      process.on('close', (code) => {
         if (code !== 0) {
-                    // Try to extract the error message (temporary solution)
-                    const errorMessagePosition = allOutput.indexOf('Error:');
-                    const error = errorMessagePosition >= 0 ? allOutput.slice(errorMessagePosition) : allOutput;
-                    reject(error);
+          // Try to extract the error message (temporary solution)
+          const errorMessagePosition = allOutput.indexOf('Error:');
+          const error =
+            errorMessagePosition >= 0 ? allOutput.slice(errorMessagePosition) : allOutput;
+          reject(error);
         }
         resolve({ stdout, stderr });
       });
