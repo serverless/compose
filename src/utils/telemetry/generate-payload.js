39b77b97d1f26c43e6c28c61578336086f78b393
Piotr Grzesik <pj.grzesik@gmail.com>
2022-03-11 09:17:30 +0100

    feat: Initial telemetry implementation

---
+++ src/utils/telemetry/generate-payload.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const ci = require('ci-info');
+const userConfig = require('@serverless/utils/config');
+const traverse = require('traverse');
+
+module.exports = ({ command, options, configuration, componentName }) => {
+  let commandDurationMs;
+
+  if (EvalError.$composeCommandStartTime) {
+    const diff = process.hrtime(EvalError.$composeCommandStartTime);
+    // First element is in seconds and second in nanoseconds
+    commandDurationMs = Math.floor(diff[0] * 1000 + diff[1] / 1000000);
+  }
+
+  const ciName = (() => {
+    if (process.env.SERVERLESS_CI_CD) {
+      return 'Serverless CI/CD';
+    }
+
+    if (process.env.SEED_APP_NAME) {
+      return 'Seed';
+    }
+
+    if (ci.isCI) {
+      if (ci.name) {
+        return ci.name;
+      }
+      return 'unknown';
+    }
+    return null;
+  })();
+  let timezone;
+
+  try {
+    timezone = new Intl.DateTimeFormat().resolvedOptions().timeZone;
+  } catch {
+    // Pass silently
+  }
+
+  const usedVersions = (() => {
+    return {
+      '@serverless/compose': require('../../../package').version,
+    };
+  })();
+
+  const payload = {
+    command,
+    commandType: componentName ? 'single' : 'global',
+    cliName: '@serverless/compose',
+    ciName,
+    commandOptionNames: Object.keys(options),
+    frameworkLocalUserId: userConfig.get('frameworkId'),
+    timestamp: Date.now(),
+    timezone,
+    versions: usedVersions,
+  };
+
+  if (commandDurationMs != null) {
+    payload.commandDurationMs = commandDurationMs;
+  }
+
+  const variablesReferenceCount = (() => {
+    const regex = /\${\w+\.\w+}/g;
+    let referenceCount = 0;
+    traverse(configuration).forEach((value) => {
+      const matches = typeof value === 'string' ? value.match(regex) : null;
+      if (matches) {
+        referenceCount += matches.length;
+      }
+    });
+    return referenceCount;
+  })();
+  payload.config = {
+    componentsOutputsVariablesCount: variablesReferenceCount,
+    components: Object.values(configuration.services).map((serviceDefinition) => {
+      const dependsOnCount = (() => {
+        if (!serviceDefinition.dependsOn) return 0;
+        if (typeof serviceDefinition.dependsOn === 'string') return 1;
+        return serviceDefinition.dependsOn.length;
+      })();
+      return {
+        type: serviceDefinition.type || 'serverless-framework',
+        dependsOnCount,
+        paramsCount: Object.values(serviceDefinition.params || {}).length,
+      };
+    }),
+  };
+
+  const commandTargetComponents = (() => {
+    if (componentName) {
+      return [configuration.services[componentName].type || 'serverless-framework'];
+    }
+    return Object.values(configuration.services).map(
+      (serviceDefinition) => serviceDefinition.type || 'serverless-framework'
+    );
+  })();
+
+  payload.commandTargetComponents = commandTargetComponents;
+
+  return payload;
+};
---
+++ src/utils/telemetry/store-locally.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const telemetryUrl = require('@serverless/utils/analytics-and-notfications-url');
+const isTelemetryDisabled = require('./is-disabled');
+const cacheDirPath = require('./cache-path');
+const { join } = require('path');
+const { v1: uuid } = require('uuid');
+const fse = require('fs-extra');
+const ensurePlainObject = require('type/plain-object/ensure');
+
+// This method is explicitly kept as synchronous. The reason for it being the fact that in the future in will need to be
+// be executed in such manner due to its potential use in `process.on('SIGINT')` handler.
+function storeLocally(payload, context) {
+  ensurePlainObject(payload);
+  if (!telemetryUrl || isTelemetryDisabled || !cacheDirPath) return null;
+  const id = uuid();
+
+  return (function self() {
+    try {
+      return fse.writeJsonSync(join(cacheDirPath, id), { payload, timestamp: Date.now() });
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        try {
+          fse.ensureDirSync(cacheDirPath);
+          return self();
+        } catch (ensureDirError) {
+          context.logger.verbose('Cache dir creation error:', ensureDirError);
+        }
+      }
+      context.logger.verbose(`Write cache file error: ${id}`, error);
+      return null;
+    }
+  })();
+}
+
+module.exports = storeLocally;
