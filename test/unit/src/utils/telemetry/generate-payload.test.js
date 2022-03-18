1d8b87eff574c9a035e0d5cc04d7e8256e41863c
Piotr Grzesik <pj.grzesik@gmail.com>
2022-03-18 15:11:11 +0100

    feat: Recognize errors in payload

---
+++ test/unit/src/utils/telemetry/generate-payload.test.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const expect = require('chai').expect;
+const path = require('path');
+
+const Context = require('../../../../../src/Context');
+const ServerlessError = require('../../../../../src/serverless-error');
+const generatePayload = require('../../../../../src/utils/telemetry/generate-payload');
+
+const versions = {
+  '@serverless/compose': require('../../../../../package').version,
+};
+
+describe('test/unit/lib/utils/telemetry/generate-payload.test.js', () => {
+  it('generates expected payload', () => {
+    const contextConfig = {
+      root: process.cwd(),
+      stateRoot: path.join(process.cwd(), '.serverless'),
+      stage: 'dev',
+      appName: 'some-random-name',
+      interactiveDisabled: true,
+    };
+    const context = new Context(contextConfig);
+    const payload = generatePayload({
+      command: 'deploy',
+      options: { someoption: 'abc' },
+      configuration: {
+        name: 'test-service',
+        services: {
+          resources: {
+            path: 'resources',
+          },
+          consumer: {
+            path: 'consumer',
+            params: {
+              workerQueueArn: '${resources.WorkerQueueArn}',
+            },
+          },
+        },
+      },
+      componentName: 'resources',
+      context,
+    });
+    expect(payload).to.have.property('frameworkLocalUserId');
+    delete payload.frameworkLocalUserId;
+    expect(payload).to.have.property('timestamp');
+    delete payload.timestamp;
+    expect(payload).to.have.property('timezone');
+    delete payload.timezone;
+    expect(payload).to.have.property('ciName');
+    delete payload.ciName;
+
+    expect(payload).to.deep.equal({
+      cliName: '@serverless/compose',
+      command: 'deploy',
+      commandOptionNames: ['someoption'],
+      commandTargetComponents: ['serverless-framework'],
+      commandType: 'single',
+      componentsOutcomes: [],
+      config: {
+        components: [
+          {
+            dependsOnCount: 0,
+            paramsCount: 0,
+            type: 'serverless-framework',
+          },
+          {
+            dependsOnCount: 0,
+            paramsCount: 1,
+            type: 'serverless-framework',
+          },
+        ],
+        componentsOutputsVariablesCount: 1,
+      },
+      outcome: 'unrecognized',
+      versions,
+    });
+  });
+
+  it('recognizes programmer error in telemetry', () => {
+    const contextConfig = {
+      root: process.cwd(),
+      stateRoot: path.join(process.cwd(), '.serverless'),
+      stage: 'dev',
+      appName: 'some-random-name',
+      interactiveDisabled: true,
+    };
+    const context = new Context(contextConfig);
+    const error = new Error('some error without code');
+    const payload = generatePayload({
+      command: 'deploy',
+      options: { someoption: 'abc' },
+      configuration: {
+        name: 'test-service',
+        services: {
+          resources: {
+            path: 'resources',
+          },
+        },
+      },
+      componentName: 'resources',
+      context,
+      error,
+    });
+
+    expect(payload.failureReason.kind).to.equal('programmer');
+    expect(payload.failureReason).to.have.property('location');
+  });
+
+  it('recognizes user error in telemetry', () => {
+    const contextConfig = {
+      root: process.cwd(),
+      stateRoot: path.join(process.cwd(), '.serverless'),
+      stage: 'dev',
+      appName: 'some-random-name',
+      interactiveDisabled: true,
+    };
+    const context = new Context(contextConfig);
+    const error = new ServerlessError('some error with code', 'ERROR_CODE');
+    const payload = generatePayload({
+      command: 'deploy',
+      options: { someoption: 'abc' },
+      configuration: {
+        name: 'test-service',
+        services: {
+          resources: {
+            path: 'resources',
+          },
+        },
+      },
+      componentName: 'resources',
+      context,
+      error,
+    });
+
+    expect(payload.failureReason.kind).to.equal('user');
+    expect(payload.failureReason.code).to.equal('ERROR_CODE');
+  });
+});
