4fa0b717c5556dfb6096ff20d5b8b50335aafb2b
Matthieu Napoli <matthieu@mnapoli.fr>
2022-04-05 19:33:12 +0200

    feat: Always write verbose logs to `.serverless/compose.log`
    Verbose logs will now be accessible through a file, even if the command was not running in verbose.
    
    The goal is to allow users to more easily debug problems by having logs always available after the fact.

--- src/cli/Logger.js
+++ src/cli/Logger.js
@@ -3,16 +3,24 @@
 const colors = require('./colors');
 const symbols = require('./symbols');
 const { safeWrite } = require('./output');
+const fs = require('fs');
+const stripAnsi = require('strip-ansi');
+const path = require('path');
 
 class Logger {
   /** @type {Array<{ namespace?: string[], message: string }>} */
   verboseLogs = [];
+  logsFilePath = '.serverless/compose.log';
 
   /**
    * @param {boolean} verboseMode
+   * @param {Record<string, NodeJS.WritableStream>} [streams] To allow mocking in tests
    */
-  constructor(verboseMode) {
+  constructor(verboseMode, streams) {
     this.verboseMode = verboseMode;
+    this.stdout = streams?.stdout ? streams.stdout : process.stdout;
+    this.stderr = streams?.stderr ? streams.stderr : process.stderr;
+    this.logsFileStream = streams?.logsFileStream ? streams.logsFileStream : this.openLogsFile();
   }
 
   /**
@@ -21,7 +29,9 @@ class Logger {
    * @param {string[]} [namespace]
    */
   writeText(message, namespace = []) {
-    safeWrite(message, process.stdout, this.generatePrefix(namespace));
+    message = this.namespaceLogMessage(message, namespace);
+    safeWrite(message, this.stdout);
+    this.writeToLogsFile(message);
   }
 
   /**
@@ -30,7 +40,9 @@ class Logger {
    * @param {string[]} [namespace]
    */
   log(message, namespace = []) {
-    safeWrite(message, process.stderr, this.generatePrefix(namespace));
+    message = this.namespaceLogMessage(message, namespace);
+    safeWrite(message, this.stderr);
+    this.writeToLogsFile(message);
   }
 
   /**
@@ -39,8 +51,11 @@ class Logger {
    */
   verbose(message, namespace) {
     if (!message || message === '') return;
+
+    this.writeToLogsFile(message);
+
     if (this.verboseMode) {
-      this.log(colors.gray(message), namespace);
+      this.doLogVerbose(message, namespace);
     } else {
       this.verboseLogs.push({
         message,
@@ -67,22 +82,68 @@ class Logger {
     this.verboseMode = true;
     // Flush all previous verbose logs to the output
     this.verboseLogs.forEach(({ message, namespace }) => {
-      this.verbose(message, namespace);
+      this.doLogVerbose(message, namespace);
     });
     this.verboseLogs = [];
   }
 
   /**
    * @private
-   * @param {string[]} namespace
+   * @param {string} message
+   * @param {string[]} [namespace]
+   */
+  doLogVerbose(message, namespace) {
+    message = colors.gray(message);
+    message = this.namespaceLogMessage(message, namespace);
+    safeWrite(message, this.stderr);
+  }
+
+  /**
+   * @private
+   * @param {string} text
+   */
+  writeToLogsFile(text) {
+    text = stripAnsi(text);
+    this.logsFileStream.write(`${text}\n`);
+  }
+
+  /**
+   * @private
+   * @param {string|undefined} text
+   * @param {string[]} [namespace]
+   * @return {string}
+   */
+  namespaceLogMessage(text, namespace) {
+    text = text ?? '';
+    const prefix = this.generatePrefix(namespace);
+    return text
+      .split('\n')
+      .map((line) => `${prefix}${line}`)
+      .join('\n');
+  }
+
+  /**
+   * @private
+   * @param {string[]} [namespace]
    * @return string
    */
   generatePrefix(namespace) {
+    if (!namespace) {
+      return '';
+    }
     let prefix = namespace.join(` ${symbols.separator} `);
     if (prefix.length > 0) {
       prefix = colors.gray(`${prefix} ${symbols.separator} `);
     }
     return prefix;
+  }
+
+  openLogsFile() {
+    const directory = path.dirname(this.logsFilePath);
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+    }
+    return fs.createWriteStream(this.logsFilePath);
   }
 }
 
