9bcf0a6f74c7e6dbe8707c65a9c202ba7d5012a8
Matthieu Napoli <matthieu@mnapoli.fr>
2022-04-05 19:33:12 +0200

    feat: Always write verbose logs to `.serverless/compose.log`
    Verbose logs will now be accessible through a file, even if the command was not running in verbose.
    
    The goal is to allow users to more easily debug problems by having logs always available after the fact.

--- src/cli/output.js
+++ src/cli/output.js
@@ -4,22 +4,28 @@
  * Safely writes text to a stream.
  * It takes into account dynamic content that may be written below.
  * @param {string} text
- * @param {NodeJS.WriteStream} stream
- * @param {string} [prefix] Prefix to add before each line
+ * @param {NodeJS.WritableStream | NodeJS.WriteStream} stream
  */
-function safeWrite(text, stream, prefix = '') {
+function safeWrite(text, stream) {
   text = text ?? '';
   for (const line of text.split('\n')) {
     // This writes from the cursor
-    stream.write(`${prefix}${line}`);
+    stream.write(line);
     // But maybe the line already contained content (e.g. a progress)
     // so we clear the rest of line, up till its end (on the right side)
-    if (stream.clearLine) {
+    if (isInteractiveStream(stream)) {
       stream.clearLine(1);
     }
     // Then we can add a line return
     stream.write('\n');
   }
+}
+
+/**
+ * @return {stream is NodeJS.WriteStream}
+ */
+function isInteractiveStream(stream) {
+  return typeof stream.clearLine === 'function';
 }
 
 module.exports = { safeWrite };
---
+++ test/unit/src/cli/Logger.test.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const { PassThrough } = require('stream');
+const expect = require('chai').expect;
+const Logger = require('../../../../src/cli/Logger');
+const colors = require('../../../../src/cli/colors');
+
+describe('test/unit/lib/cli/Logger.test.js', () => {
+  /** @type {Logger} */
+  let logger;
+  beforeEach(() => {
+    logger = new Logger(false, mockedStreams());
+  });
+
+  it('writes text', async () => {
+    logger.writeText('Message');
+
+    expect(await readStream(logger.stdout)).to.equal('Message\n');
+    expect(await readStream(logger.stderr)).to.equal('');
+    expect(await readStream(logger.logsFileStream)).to.equal('Message\n');
+  });
+
+  it('can namespace text', async () => {
+    logger.writeText('Message with\nmultiple lines', ['foo', 'bar']);
+
+    expect(await readStream(logger.stdout)).to.equal(
+      `${colors.gray('foo › bar › ')}Message with\n` +
+        // We check that the namespace is applied to all lines
+        `${colors.gray('foo › bar › ')}multiple lines\n`
+    );
+    expect(await readStream(logger.stderr)).to.equal('');
+    expect(await readStream(logger.logsFileStream)).to.equal(
+      'foo › bar › Message with\nfoo › bar › multiple lines\n'
+    );
+  });
+
+  it('strips colors when logging into the log file', async () => {
+    logger.writeText(colors.gray('Message'));
+
+    // Logged with colors on stdout
+    expect(await readStream(logger.stdout)).to.equal(`${colors.gray('Message')}\n`);
+    expect(await readStream(logger.stderr)).to.equal('');
+    // But written without colors into the log file
+    expect(await readStream(logger.logsFileStream)).to.equal('Message\n');
+  });
+
+  it('can hold verbose logs', async () => {
+    logger.verbose('Message');
+
+    expect(await readStream(logger.stdout)).to.equal('');
+    // Verbose logs are NOT written to stderr
+    expect(await readStream(logger.stderr)).to.equal('');
+    // But they are written to the log file
+    expect(await readStream(logger.logsFileStream)).to.equal('Message\n');
+  });
+
+  it('can write verbose logs', async () => {
+    logger = new Logger(true, mockedStreams());
+
+    logger.verbose('Message');
+
+    expect(await readStream(logger.stdout)).to.equal('');
+    // Verbose logs are written (with colors) to stderr
+    expect(await readStream(logger.stderr)).to.equal(`${colors.gray('Message')}\n`);
+    // And (without colors) to the log file
+    expect(await readStream(logger.logsFileStream)).to.equal('Message\n');
+  });
+
+  it('can switch to verbose logs at runtime', async () => {
+    logger.verbose('Message 1');
+    logger.enableVerbose();
+    logger.verbose('Message 2');
+
+    expect(await readStream(logger.stdout)).to.equal('');
+    expect(await readStream(logger.stderr)).to.equal(
+      `${colors.gray('Message 1')}\n${colors.gray('Message 2')}\n`
+    );
+    expect(await readStream(logger.logsFileStream)).to.equal('Message 1\nMessage 2\n');
+  });
+
+  it('logs errors', async () => {
+    logger.error('A text error');
+    logger.error(new Error('An error'));
+
+    expect(await readStream(logger.stdout)).to.equal('');
+    expect(await readStream(logger.stderr)).to.equal(
+      `${colors.red('Error:')} A text error\n${colors.red('Error:')} An error\n`
+    );
+    expect(await readStream(logger.logsFileStream)).to.equal(
+      'Error: A text error\nError: An error\n'
+    );
+  });
+
+  it('logs errors with stack traces in verbose', async () => {
+    logger = new Logger(true, mockedStreams());
+
+    logger.error(new TypeError('An error'));
+
+    expect(await readStream(logger.stdout)).to.equal('');
+    expect(await readStream(logger.stderr)).to.contain(
+      `${colors.red('Error:')} TypeError: An error\n    at Context.<anonymous>`
+    );
+    expect(await readStream(logger.logsFileStream)).to.contain(
+      'Error: TypeError: An error\n    at Context.<anonymous>'
+    );
+  });
+});
+
+function mockedStreams() {
+  return {
+    stdout: new PassThrough(),
+    stderr: new PassThrough(),
+    logsFileStream: new PassThrough(),
+  };
+}
+
+/**
+ * @param {NodeJS.WritableStream} stream
+ * @return {Promise<string>}
+ */
+function readStream(stream) {
+  stream.end();
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  });
+}
