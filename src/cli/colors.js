540daf1cfd4adaa008987441a557f92a46f61c91
Matthieu Napoli <matthieu@mnapoli.fr>
2022-04-14 11:25:57 +0200

    fix: Replace white color to support white backgrounds

--- src/cli/colors.js
+++ src/cli/colors.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 const colorSupportLevel = chalk.supportsColor ? chalk.supportsColor.level : 0;
 
 module.exports = {
-  white: chalk.white,
+  foreground: chalk.reset,
   gray: colorSupportLevel > 2 ? chalk.rgb(140, 141, 145) : chalk.gray,
   red: colorSupportLevel > 2 ? chalk.rgb(253, 87, 80) : chalk.redBright,
   warning: chalk.rgb(255, 165, 0),
