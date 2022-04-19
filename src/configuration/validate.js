3f14cd52d79c58ecad5eb906ca5e6dc98cee440c
Piotr Grzesik <pj.grzesik@gmail.com>
2022-04-19 09:52:24 +0200

    fix: Do not allow empty path or root dir path for Framework services

--- src/configuration/validate.js
+++ src/configuration/validate.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const ServerlessError = require('../serverless-error');
+const isObject = require('type/object/is');
 
 function validateConfiguration(configuration, configurationPath) {
   const configurationFilename = path.basename(configurationPath);
@@ -9,17 +10,42 @@ function validateConfiguration(configuration, configurationPath) {
     throw new ServerlessError(
       `Resolved "${configurationFilename}" does not contain valid Serverless Compose configuration.\n` +
         'Read about Serverless Compose in the documentation: https://github.com/serverless/compose',
-      'INVALID_CONFIGURATION'
+      'INVALID_NON_OBJECT_CONFIGURATION'
     );
   }
 
-  if (!configuration.services) {
+  if (!isObject(configuration.services)) {
     throw new ServerlessError(
       `Invalid configuration: "${configurationFilename}" must contain "services" property.\n` +
         'Read about Serverless Compose configuration in the documentation: https://github.com/serverless/compose',
-      'INVALID_CONFIGURATION'
+      'INVALID_NON_OBJECT_SERVICES_CONFIGURATION'
     );
   }
+
+  Object.entries(configuration.services).forEach(([key, value]) => {
+    if (!isObject(value)) {
+      throw new ServerlessError(
+        `Invalid configuration: Definition of "${key}" service must be an object.\n` +
+          'Read about Serverless Compose configuration in the documentation: https://github.com/serverless/compose',
+        'INVALID_NON_OBJECT_SERVICE_CONFIGURATION'
+      );
+    }
+
+    if (!value.path) {
+      throw new ServerlessError(
+        `Invalid configuration: Definition of "${key}" service must contain a "path" property.\n` +
+          'Read about Serverless Compose configuration in the documentation: https://github.com/serverless/compose',
+        'MISSING_PATH_IN_SERVICE_CONFIGURATION'
+      );
+    }
+
+    if (path.relative(process.cwd(), value.path) === '') {
+      throw new ServerlessError(
+        `Definition of "${key}" service must contain a "path" property that does not point to the root directory of Serverless Compose project`,
+        'INVALID_PATH_IN_SERVICE_CONFIGURATION'
+      );
+    }
+  });
 
   // Provide a targeted error message if users use Framework options
   const frameworkConfigKeys = [
