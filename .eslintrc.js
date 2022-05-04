b652af01626d1824fcae2b1e0d909c201b79b5dd
Matthieu Napoli <matthieu@mnapoli.fr>
2022-05-04 23:16:23 +0200

    chore: Add tooling to support TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  ignorePatterns: ['demo/*'],
+  plugins: ['import', '@typescript-eslint'],
+  extends: ['@serverless/eslint-config/node'],
+  overrides: [
+    // Rules specific for TypeScript
+    {
+      files: ['**/*.ts'],
+      // We must use a different parser that supports TypeScript
+      parser: '@typescript-eslint/parser',
+      // Extra rules specific to TypeScript
+      extends: [
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+  ],
+  rules: {
+    // This pattern is used by the CDK
+    'no-new': ['off']
+  },
+};
