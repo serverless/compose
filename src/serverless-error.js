25fc8ff89646af1c474b9e440c9f91f2db89a5b9
Matthieu Napoli <matthieu@mnapoli.fr>
2022-05-27 10:29:33 +0200

    feat: Allow logging the "previous" exception with `ServerlessError`

--- src/serverless-error.js
+++ src/serverless-error.js
@@ -1,9 +1,25 @@
 'use strict';
 
+/**
+ * This class is for signaling user errors.
+ */
 class ServerlessError extends Error {
-  constructor(message, code) {
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {unknown} [previous]
+   */
+  constructor(message, code, previous) {
     super(message);
     this.code = code;
+
+    // The ServerlessError class lets us pass a "previous" exception,
+    // i.e. the low-level error that we are wrapping with a better user message.
+    // We preserve the original stack trace (just like in Java or PHP)
+    // to ease debugging. See https://stackoverflow.com/q/42754270/245552
+    if (previous instanceof Error && previous.stack) {
+      this.stack += `\nFrom previous ${previous.stack}`;
+    }
   }
 }
 
